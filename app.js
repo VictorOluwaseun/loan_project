64c620f982b0ec0dffe49626699a45700af8939e
Victor <adevic4christ@gmail.com>
2019-06-29 23:55:56 +0100

    Create form

---
+++ app.js
@@ -0,0 +1,12 @@
+// jshint esversion: 6
+const lname = document.getElementById("lname"),
+    fname = document.getElementById("fname"),
+    address = document.getElementById("address"),
+    email = document.getElementById("email"),
+    occupation = document.getElementById("occupation"), 
+    amount = document.getElementById("amount"),
+    request = document.querySelector('input[type="submit"]');
+
+
+
+
