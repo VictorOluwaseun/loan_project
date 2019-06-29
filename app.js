02a7c24a7c55912c5b4ab2e6e921abf2239ba8b8
Victor <adevic4christ@gmail.com>
2019-06-29 23:58:41 +0100

    Create form

--- app.js
+++ app.js
@@ -6,7 +6,3 @@ const lname = document.getElementById("lname"),
     occupation = document.getElementById("occupation"), 
     amount = document.getElementById("amount"),
     request = document.querySelector('input[type="submit"]');
-
-
-
-
